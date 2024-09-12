af21940482ba4ae9b7007ffc9a7016e3e3996069
mohamedamineberguia <mohamedamineberguia@gmail.com>
2024-09-12 21:05:21 +0000

    rotatematrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Rotate 2D matrix'''
+
+
+def rotate_2d_matrix(matrix):
+    '''n x n 2D matrix, rotate it 90 degrees clockwise'''
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        for i in range(right - left):
+            top, bottom = left, right
+            topLeft = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topLeft
+        right -= 1
+        left += 1
